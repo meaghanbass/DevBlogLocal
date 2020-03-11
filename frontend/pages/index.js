630e6b67d4c088f16d956b7a99119d5a31350779
meaghanbass <meaghan.bass@gmail.com>
2020-03-10 20:00:15 -0700

    styles

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,12 @@
 import Layout from "../components/Layout";
+import { API, DOMAIN, APP_NAME } from '../config';
 
 const Index = () => {
     return (
         <Layout>
             <div id="index">
                 <div className="site-brand">
-                    <h1>unreal</h1>
+                    <h1>{APP_NAME}</h1>
                 </div>                
                 <div className="hero">
                     hero
